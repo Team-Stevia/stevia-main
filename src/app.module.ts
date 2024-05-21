3ef1bdf7685d9c861727b4ac9a303608a6674d4d
Jinpyo-Ahn <dkswlsvy3312@gmail.com>
2024-05-21 21:22:35 +0900

    feat: 프로젝트 환경 설정
    main 서버 프로젝트 환경 설정 진행
    
    resolve: #1

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import {
+    Module, 
+} from '@nestjs/common';
+import {
+    AppController, 
+} from './app.controller';
+import {
+    AppService, 
+} from './app.service';
+import {
+    ConfigModule, 
+} from '@nestjs/config';
+import {
+    PrismaService, 
+} from './prisma/prisma.service';
+import {
+    PrismaModule, 
+} from './prisma/prisma.module';
+
+@Module({
+    imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        cache: true,
+        envFilePath: '.env',
+    }),
+    PrismaModule,],
+    controllers: [AppController,],
+    providers: [AppService,
+        PrismaService,],
+})
+export class AppModule {
+}
