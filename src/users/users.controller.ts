6afe7863a46141a4454101a2b6310b5a3e0628ae
MoonDongmin <20191836@o365.hanbat.ac.kr>
2024-06-06 19:12:10 +0900

    feat: users API 개발
    users API 개발 진행
    - 간단한 로그인 로직 구현
    
    fixes: #3
    related to: #3

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,32 @@
 import {
-    Controller, 
+    Body,
+    Controller,
+    Post,
 } from "@nestjs/common";
+import {
+    UsersService,
+} from "./users.service";
+import {
+    UserSigninRequestDto,
+} from "./dtos/user.signin.request.dto";
 
-@Controller("users")
+@Controller("api/users")
 export class UsersController {
+    constructor(private readonly userService: UsersService) {
+    }
+
+    // 회원가입
+    @Post()
+    async signup(): Promise<any> {
+        return "회원가입";
+    }
+
+    // 로그인
+    @Post("/signin")
+    async signin(@Body() signinDto: UserSigninRequestDto): Promise<any> {
+        return await this.userService.signinUser(signinDto);
+    }
+
+    // 비밀번호 수정
+
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,33 @@
 import {
     Injectable,
 } from "@nestjs/common";
+import {
+    PrismaClient,
+} from "@prisma/client";
+import {
+    UserSigninRequestDto,
+} from "./dtos/user.signin.request.dto";
+
+const prisma = new PrismaClient();
 
 @Injectable()
 export class UsersService {
+    // 로그인
+    async signinUser(signinDto: UserSigninRequestDto): Promise<string> {
+        try {
+            const user = await prisma.user.findUnique({
+                where: {
+                    student_id: signinDto.studentId,
+                },
+            });
+
+            if (!user || user.password !== signinDto.password) {
+                return "로그인 실패";
+            }
+
+            return "로그인 성공";
+        } catch (error) {
+            throw error;
+        }
+    }
 }
